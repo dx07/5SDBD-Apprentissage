2346f9087df06396469afa277a2f560187f62c71
dx07 <dx.msr07@gmail.com>
2018-12-09 11:47:49 +0100

    MAJ TP3 + Rapport partie 3 et intro OK

--- TP3/TP3_prog1.py
+++ TP3/TP3_prog1.py
@@ -1,36 +1,159 @@
+import numpy
 from time import time
-
-import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import svm
-from sklearn.utils import shuffle
-
 from sklearn.datasets import fetch_mldata
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix
 
 mnist = fetch_mldata('MNIST original')
+indices = numpy.random.randint(70000, size=5000)
+data = mnist.data[indices]
+target = mnist.target[indices]
 
-# indices = np.random.randint(70000, size=10000)
-# mnist.data = mnist.data[indices]
-# mnist.target = mnist.target[indices]
+xtrain, xtest, ytrain, ytest = train_test_split(data, target, train_size= 0.7)
 
-mnist.data = mnist.data[0:10000]
-mnist.target = mnist.target[0:10000]
+''' temps d'execution
 
-mnist = fetch_mldata('MNIST original')
-mnist.data, mnist.target = shuffle(mnist.data, mnist.target)
+score=[]
+execution_time=[]
 
-xtrain, xtest, ytrain, ytest = train_test_split(mnist.data, mnist.target, train_size=0.7)
+values_of_c = ['sigmoid','rbf','linear','poly']
 
-for kernel in ['linear', 'poly', 'rbf', 'sigmoid']:
-    clsvm = svm.SVC(kernel=kernel)
-    time_start = time()
+for c in values_of_c:
 
-    print("Step 1 : Fit")
-    clsvm.fit(xtrain,ytrain)
-    print("Step 2 : Prédiction")
-    prediction = clsvm.predict(xtest)
-    print("Step 3 : Score")
-    score = clsvm.score(xtest, ytest)
+    clasifier = svm.SVC(kernel=c)
 
-    print("Kernel : {}, Temps : {}, Prédiction : {}, Valeur : {}, Efficacité : {}".format(kernel, time()-time_start, prediction[4], ytest[4], score))
+    begin = time()
+    clasifier.fit(xtrain,ytrain)
+    predicted = clasifier.predict(X=xtest)
+    end = time()
 
+    total_time = end - begin
+    execution_time.append(total_time)
+    print("temps d'execution: ", total_time)
+
+    # print("time: ", total_time)
+
+plt.plot(values_of_c,execution_time)
+plt.show()
+'''
+
+''' score
+
+score=[]
+execution_time=[]
+
+values_of_c = ['sigmoid','rbf','linear','poly']
+
+for c in values_of_c:
+
+    clasifier = svm.SVC(kernel=c)
+
+    begin = time()
+    clasifier.fit(xtrain,ytrain)
+    predicted = clasifier.predict(X=xtest)
+    end = time()
+
+    score.append(clasifier.score(xtest, ytest))
+    print("score: ", clasifier.score(xtest, ytest))
+
+    #total_time = end - begin
+    #print("time: ", total_time)
+
+plt.plot(values_of_c,score)
+plt.show()
+'''
+
+'''
+#variation C
+
+score=[]
+execution_time=[]
+
+values_of_c = numpy.linspace(0.1,1, 5, endpoint=True)
+
+for c in values_of_c:
+
+    clasifier = svm.SVC(kernel='rbf', C=c)
+
+    begin = time()
+    clasifier.fit(xtrain,ytrain)
+    predicted = clasifier.predict(X=xtest)
+    end = time()
+    total_time = end - begin
+
+    score.append(clasifier.score(xtest, ytest))
+    execution_time.append(total_time)
+    # print("score: ", clasifier.score(xtest, ytest))
+    # print("time: ", total_time)
+
+plt.plot(values_of_c,execution_time)
+plt.show()
+plt.plot(values_of_c,score)
+plt.show()
+'''
+
+''''# variation gamma
+
+score=[]
+execution_time=[]
+
+values_of_c = numpy.linspace(0.1,1, 5, endpoint=True)
+
+for c in values_of_c:
+
+    clasifier = svm.SVC(kernel='rbf', gamma=c)
+
+    begin = time()
+    clasifier.fit(xtrain,ytrain)
+    predicted = clasifier.predict(X=xtest)
+    end = time()
+    total_time = end - begin
+
+    score.append(clasifier.score(xtest, ytest))
+    execution_time.append(total_time)
+    # print("score: ", clasifier.score(xtest, ytest))
+    # print("time: ", total_time)
+
+plt.plot(values_of_c,score)
+plt.show()
+plt.plot(values_of_c,execution_time)
+plt.show()
+'''
+
+'''avec les paramètres générés tout seul    
+parameters = {'kernel':('linear', 'rbf', 'sigmoid', 'poly'), 'C':[1, 10]}
+
+svc = svm.SVC(kernel='sigmoid')
+
+clasifier = GridSearchCV(svc,parameters)
+begin = time()
+clasifier.fit(xtrain, ytrain)
+predicted = clasifier.predict(X=xtest)
+end = time()
+
+score.append(clasifier.score(xtest, ytest))
+print("score: ", clasifier.score(xtest, ytest))
+
+total_time = end - begin
+print("time: ", total_time)
+'''
+
+''' matrice de confusion
+clasifier = svm.SVC(kernel='linear')
+
+begin = time()
+clasifier.fit(xtrain,ytrain)
+predicted = clasifier.predict(X=xtest)
+end = time()
+
+score.append(clasifier.score(xtest, ytest))
+print("score: ", clasifier.score(xtest, ytest))
+
+total_time = end - begin
+print("time: ", total_time)
+
+print(confusion_matrix(ytest,predicted))
+'''
